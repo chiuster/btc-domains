84e9cf3cb8a8db0463521c229e080a7140848864
Ryan Chiu <rchiu6@uic.edu>
2017-11-04 19:52:43 -0500

    Trim empty array values and add contact info to top

--- index.php
+++ index.php
@@ -1,3 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Cryptocurrency Domains</title>
+  </head>
+  <body>
+    <h1>Cryptocurrency Domains for Sale</h1>
+    Any inquiries can be addressed to Chris via email to <a href="mailto:chris@strategydomains.com">chris@strategydomains.com</a> or over the phone at (650)-690-2583 during business hours (Mon-Fri 8AM-5PM PDT).
 <?php
   // header("Location: index.html");
 
@@ -5,8 +13,10 @@
   $domain_array = explode("\n", $text);
   sort($domain_array);
 
-  foreach ($domain_array as &$domain) {
+  foreach (array_filter($domain_array) as &$domain) {
     print("<a href='http://www.afternic.com/domain/$domain'>$domain</a><br />");
   }
 
 ?>
+  </body>
+</html>
