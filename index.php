8ff922133a122207363725607152579aae2c10d1
Ryan Chiu <rchiu6@uic.edu>
2017-11-04 19:45:12 -0500

    Plain text website revision

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
-  header("Location: index.html");
+  // header("Location: index.html");
+
+  $text = file_get_contents("list");
+  $domain_array = explode("\n", $text);
+  rsort($domain_array);
+
+  foreach ($domain_array as &$domain) {
+    print("<a href='http://www.afternic.com/domain/'".$domain.">".$domain."</a><br />");
+  }
+
 ?>
